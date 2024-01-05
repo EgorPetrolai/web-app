00cb61d8b4268d7c12ce7c21457a459a33409e46
Egor <lindos228@gmail.com>
2024-01-05 11:59:25 +0300

    after install django

--- PersonalFinance/PersonalFinance/settings.py
+++ PersonalFinance/PersonalFinance/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main',
     'wallets',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PersonalFinance/PersonalFinance/urls.py
+++ PersonalFinance/PersonalFinance/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('wallets/', include('wallets.urls')),
+    path('news/', include('news.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
