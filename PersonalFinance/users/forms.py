657b407225172a533a7da780e9036a967a1f1141
Egor <lindos228@gmail.com>
2024-01-23 19:02:31 +0300

    domeiko

--- PersonalFinance/users/forms.py
+++ PersonalFinance/users/forms.py
@@ -1,24 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2')
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'userform'
-        self.fields['email'].widget.attrs['class'] = 'userform'
-        self.fields['password1'].widget.attrs['class'] = 'userform'
-        self.fields['password2'].widget.attrs['class'] = 'userform'
-
-class SignInForm(AuthenticationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'password')
-
-    def __init__(self, *args, **kwargs):
-        super(SignInForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'userform'
-        self.fields['password'].widget.attrs['class'] = 'userform'
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
